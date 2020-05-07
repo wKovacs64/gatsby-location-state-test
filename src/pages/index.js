6daa79e07b95f3c98f6654d7b2ed9fb6201b49b1
Justin Hall <justin.r.hall@gmail.com>
2020-05-07 10:56:32 -0600

    chore: remove unrelated stuff

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,12 @@
 import React from "react"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
 
 const IndexPage = ({ navigate }) => (
   <Layout>
-    <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
     <button
       type="button"
       onClick={() => navigate("page-2", { state: { success: true } })}
